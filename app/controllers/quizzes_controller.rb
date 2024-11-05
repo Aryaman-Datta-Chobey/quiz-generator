65532fb644543ad1937215add1ec29d8ee689395
thanhdang2712 <danghuyenthanh2003@gmail.com>
2024-11-04 22:53:38 -0500

    Generate a template for the controller

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,2 +1,59 @@
 class QuizzesController < ApplicationController
-end
+    before_action :set_quiz, only: [:show, :edit, :update, :destroy]
+  
+    # GET /quizzes
+    def index
+      @quizzes = Quiz.all
+    end
+  
+    # GET /quizzes/:id
+    def show
+    end
+  
+    # GET /quizzes/new
+    def new
+      @quiz = Quiz.new
+    end
+  
+    # POST /quizzes
+    def create
+      @quiz = Quiz.new(quiz_params)
+      if @quiz.save
+        redirect_to @quiz, notice: 'Quiz was successfully created.'
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  
+    # GET /quizzes/:id/edit
+    def edit
+    end
+  
+    # PATCH/PUT /quizzes/:id
+    def update
+      if @quiz.update(quiz_params)
+        redirect_to @quiz, notice: 'Quiz was successfully updated.'
+      else
+        render :edit, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /quizzes/:id
+    def destroy
+      @quiz.destroy
+      redirect_to quizzes_url, notice: 'Quiz was successfully deleted.'
+    end
+  
+    private
+  
+    # Find quiz by ID for show, edit, update, destroy actions
+    def set_quiz
+      @quiz = Quiz.find(params[:id])
+    end
+  
+    # Only allow a list of trusted parameters through
+    def quiz_params
+      params.require(:quiz).permit(:topic, :difficulty, :study_duration, :detail_level, :number_of_questions, :score)
+    end
+  end
+  
