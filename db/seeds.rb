0a025d78410f4130490f67a75449f48beae0fd56
thanhdang2712 <danghuyenthanh2003@gmail.com>
2024-11-04 22:51:31 -0500

    Generate routes, controllers, models

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,24 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+quiz = Quiz.create!(
+  topic: "Ruby on Rails Basics",
+  difficulty: "Intermediate",
+  study_duration: 30,
+  detail_level: "Detailed",
+  number_of_questions: 2,
+  score: 0
+)
+# Create questions associated with the quiz
+Question.create!(
+  quiz: quiz,
+  content: "What is Rails?",
+  options: ["A web application framework", "A programming language", "A database", "A front-end framework"],
+  correct_answer: "A web application framework"
+)
+Question.create!(
+  quiz: quiz,
+  content: "Which command is used to generate a new Rails model?",
+  options: ["rails create model", "rails generate model", "rails new model", "rails model generate"],
+  correct_answer: "rails generate model"
+)
