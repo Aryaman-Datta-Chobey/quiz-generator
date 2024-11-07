26cdbf7ec9e8746ecbcf566a181550bdd3d245f6
stefanomonte <guyderf@gmail.com>
2024-11-06 19:34:42 -0500

    Added filtering index test

--- spec/views/index.html.erb_spec.rb
+++ spec/views/index.html.erb_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe "quizzes/index.html.erb", type: :view do
+
+
+RSpec.describe "Index Route", type: :view do
+  before do
+    driven_by(:rack_test)
+  end
+
   it "should render a quiz" do
     assign(:quizzes, [ Quiz.new(topic: "Rspec Testing",
                              difficulty: "easy",
@@ -15,7 +21,16 @@ RSpec.describe "quizzes/index.html.erb", type: :view do
                              number_of_questions: 100,
                              score: 0) ])
     render
-    expect(rendered).to match(/Rspec Testing/)
-    expect(rendered).to match(/Passing Rspec Tests/)
+    expect(page.text).to match(/Rspec Testing/)
+    expect(page.text).to match(/Passing Rspec Tests/)
+    expect(page.text).to match(/Rspec Testing.*Passing Rspec Tests/mi)
+  end
+
+  it 'should filter quizzes by search string' do
+      visit quizzes_path
+      fill_in 'query', with: 'Rails'
+      click_button 'Find Quiz'
+      expect(page.text).to match(/Rspec Testing/i)
+      expect(page.text).not_to match(/Passing Rspec Tests/i)
   end
 end
