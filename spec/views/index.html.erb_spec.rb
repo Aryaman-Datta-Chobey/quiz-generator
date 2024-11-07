4e1111c13d41a4c0b8e172a8272b17fd539c5c12
stefanomonte <guyderf@gmail.com>
2024-11-06 19:22:15 -0500

    Fixed incorreclty implemented index test objects

--- spec/views/index.html.erb_spec.rb
+++ spec/views/index.html.erb_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 RSpec.describe "quizzes/index.html.erb", type: :view do
   it "should render a quiz" do
     assign(:quizzes, [ Quiz.new(topic: "Rspec Testing",
-                             difficulty: "Easy",
+                             difficulty: "easy",
                              study_duration: 9,
-                             detail_level: "High",
+                             detail_level: "high",
                              number_of_questions: 100,
                              score: 0),
                      Quiz.new(topic: "Passing Rspec Tests",
-                             difficulty: "Hard",
+                             difficulty: "hard",
                              study_duration: 10,
-                             detail_level: "High",
+                             detail_level: "high",
                              number_of_questions: 100,
                              score: 0) ])
     render
