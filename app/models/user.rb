ff3277fabe3ab135550b3c5623e1919b09feca5a
thanhdang2712 <danghuyenthanh2003@gmail.com>
2024-11-07 21:40:26 -0500

    Add and generate User devise gem dependencies

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
-    has_many :quizzes, dependent: :destroy
-    has_secure_password
-  
-    # validates :username, presence: true, uniqueness: true
-    # validates :email, presence: true, uniqueness: true
-  end
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
