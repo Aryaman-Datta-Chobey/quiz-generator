29b94e031d94d2b8c9663dcf62b0f051395b0d19
Aryaman-Datta-Chobey <97906694+Aryaman-Datta-Chobey@users.noreply.github.com>
2024-12-12 05:51:16 +0000

    turbo wrangling

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,7 @@
 class QuestionsController < ApplicationController
-  before_action :authenticate_user!, only: %i[create]
-  before_action :set_quiz, only: [:create]
+  before_action :authenticate_user!, only: %i[create show edit update destroy]
+  before_action :set_quiz, only: %i[create show edit update destroy]
+  before_action :set_question, only: %i[show edit update destroy]
   rescue_from ActiveRecord::RecordNotFound, with: :handle_bad_id
 
   # POST /quizzes/:quiz_id/questions
@@ -10,13 +11,34 @@ class QuestionsController < ApplicationController
       @quiz.update(number_of_questions: @quiz.questions.count) #increment question count to reflect new question
       respond_to do |format|
         format.turbo_stream
+        format.html { redirect_to edit_quiz_path(@quiz), notice: "Question added successfully." }
       end  
     else
       flash[:alert] = "There was an error adding the question."
-      #render 'quizzes/show', status: :unprocessable_entity
+      render :new, status: :unprocessable_entity
     end
   end
 
+  def edit; end
+
+  def update
+    if @question.update(question_params)
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_to edit_quiz_path(@quiz), notice: "Question updated successfully." }
+      end
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @question.destroy
+    respond_to do |format|
+      format.turbo_stream
+      format.html { redirect_to edit_quiz_path(@quiz), notice: "Question deleted successfully." }
+    end
+  end
   private
 
   def set_quiz
@@ -25,6 +47,10 @@ class QuestionsController < ApplicationController
 
   def question_params
     params.require(:question).permit(:content, :options, :correct_answer)
+  end
+
+  def set_question
+    @question = @quiz.questions.find(params[:id])
   end
 
   def handle_bad_id
